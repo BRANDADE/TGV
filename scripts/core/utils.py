b6570a4cd8cabce2efa44d7f4991f8e92a8be5d9
Corentin Marco <corentin.marco@chu-nimes.fr>
2026-08-10 09:30:18 +0200

    fix:run_id

--- scripts/core/utils.py
+++ scripts/core/utils.py
@@ -6,5 +6,8 @@ def get_analysis_prefix(zip_path):
     Ex : /path/to/RUN_2025_05_21.zip → RUN_2025_05_21
     """
     base = os.path.basename(zip_path)
-    prefix = base.replace(".zip", "")
+    if base.endswith("-trgt_vcfs.zip"):
+        prefix = base.replace("-trgt_vcfs.zip", "")
+    else:
+        prefix = base.replace(".zip", "") 
     return prefix
